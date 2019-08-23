4597d4d2f427336cf366cb34962c2818633e9785
rmamchyk <roman.mamchyk@gmail.com>
2019-08-23 22:46:56 +0300

    Initial commit

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,66 @@
+import {GraphQLServer} from 'graphql-yoga';
+
+// Scalar types: String, Boolean, Int, Float, ID
+
+// Type definitions (schema)
+const typeDefs = `
+    type Query {
+        greeting(name: String): String!
+        add(a: Float!, b: Float!): Float!
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+    }
+`;
+
+// Resolvers
+const resolvers = {
+    Query: {
+        greeting(parent, args, ctx, info) {
+            if (args.name) {
+                return `Hello ${args.name}!`
+            }
+            return 'Hello!'
+        },
+        add(parent, args, ctx, info) {
+            return args.a + args.b;
+        },
+        me() {
+            return {
+                id: "123098",
+                name: 'Mike',
+                email: 'mike@example.com'
+            }
+        },
+        post() {
+            return {
+                id: '092',
+                title: 'GraphQL 101',
+                body: '',
+                published: false
+            }
+        }
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => {
+    console.log('The server is up!');
+});
